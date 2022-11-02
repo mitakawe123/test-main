10443a7abdf3c013ea4651260df387f92e7a0227
mitakawe123 <dkyuchukov2003@gmail.com>
2022-11-02 19:30:14 +0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,24 @@ import { Platform } from '@angular/cdk/platform';
 })
 export class AppComponent implements OnInit {
   title = 'test-main';
+
   modalPwaEvent: any;
   modalPwaPlatform: string | undefined;
 
-  constructor(private platform: Platform) {
-  }
+  constructor(private platform: Platform) {}
 
   public ngOnInit(): void {
     this.loadModalPwa();
   }
 
   private loadModalPwa(): void {
-    if (this.platform.ANDROID || this.platform.isBrowser) {
-      window.addEventListener('beforeinstallprompt', (event: any) => {
-        event.preventDefault();
-        this.modalPwaEvent = event;
-        event.userChoice.then((choiceResult: any) => {
-          console.log(choiceResult.outcome); // either "accepted" or "dismissed"
-        });
+    window.addEventListener('beforeinstallprompt', (event: any) => {
+      event.preventDefault();
+      this.modalPwaEvent = event;
+      event.userChoice.then((choiceResult: any) => {
+        console.log(choiceResult.outcome); // either "accepted" or "dismissed"
       });
-    }
+    });
 
     // if (this.platform.IOS && this.platform.SAFARI) {
     //   const isInStandaloneMode =
