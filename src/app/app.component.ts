6610a68e151d1d153ec54166a375195fb6b0a7fb
mitakawe123 <dkyuchukov2003@gmail.com>
2022-11-02 19:15:37 +0200

    dsadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,7 +57,7 @@ export class AppComponent implements OnInit {
   }
 
   private loadModalPwa(): void {
-    if (this.platform.ANDROID) {
+    if (this.platform.ANDROID || this.platform.isBrowser) {
       window.addEventListener('beforeinstallprompt', (event: any) => {
         event.preventDefault();
         this.modalPwaEvent = event;
