57270e2928dd19458903039107d1cd6a302b1279
mitakawe123 <dkyuchukov2003@gmail.com>
2022-11-02 19:23:33 +0200

    ddd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,10 @@ import { Platform } from '@angular/cdk/platform';
 })
 export class AppComponent implements OnInit {
   title = 'test-main';
-  isOnline: boolean;
-  modalVersion: boolean;
   modalPwaEvent: any;
   modalPwaPlatform: string | undefined;
 
   constructor(private platform: Platform) {
-    this.isOnline = false;
-    this.modalVersion = false;
   }
 
   public ngOnInit(): void {
@@ -26,26 +22,24 @@ export class AppComponent implements OnInit {
       window.addEventListener('beforeinstallprompt', (event: any) => {
         event.preventDefault();
         this.modalPwaEvent = event;
-        this.modalPwaPlatform = 'ANDROID';
         event.userChoice.then((choiceResult: any) => {
           console.log(choiceResult.outcome); // either "accepted" or "dismissed"
         });
       });
     }
 
-    if (this.platform.IOS && this.platform.SAFARI) {
-      const isInStandaloneMode =
-        'standalone' in window.navigator &&
-        (<any>window.navigator)['standalone'];
-      if (!isInStandaloneMode) {
-        this.modalPwaPlatform = 'IOS';
-      }
-    }
+    // if (this.platform.IOS && this.platform.SAFARI) {
+    //   const isInStandaloneMode =
+    //     'standalone' in window.navigator &&
+    //     (<any>window.navigator)['standalone'];
+    //   if (!isInStandaloneMode) {
+    //     this.modalPwaPlatform = 'IOS';
+    //   }
+    // }
   }
 
   public addToHomeScreen(): void {
     this.modalPwaEvent.prompt();
-
     this.modalPwaPlatform = undefined;
   }
 
